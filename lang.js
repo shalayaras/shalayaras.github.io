554d562d676be35f4e928fe4382d78ef7eb52ef3
shalayaras <parwez209aras@gmail.com>
2026-02-22 22:34:08 +0300

    lang.js
    Save the selected language to localStorage and load it on page load.

--- lang.js
+++ lang.js
@@ -8,4 +8,10 @@ const text = {
 function setLang(l){
   document.getElementById("name").innerText = text[l].name;
   document.getElementById("bio").innerText = text[l].bio;
+  localStorage.setItem("lang", l);
 }
+
+window.onload = function(){
+  const savedLang = localStorage.getItem("lang") || "en";
+  setLang(savedLang);
+};
